a4414d28c2e9da3f9200bb92364a73bdc7b2a10e
kdpandey09 <pandeykaustubdutt@gmail.com>
2022-02-07 16:31:38 +0530

    Varags in java

---
+++ fourth.java
@@ -0,0 +1,28 @@
+/* Program to illustrate Variable Arguments (Varargs) in Java  (KAUSTUB DUTT PANDEY) */
+class calc{
+    void add(int... a){
+        int sum = 0;
+        for(int i : a){
+            sum = sum + i;
+        }
+        System.out.println("Sum of given arguments is = "+ sum);
+    }
+    void multiply(int... a){
+        int temp = 1;
+        for(int i : a){
+         temp = temp * i;
+        }
+        System.out.println("mutiplication of given arguments is = "+ temp);
+    }
+    void display(int... a){
+        for(int i : a){
+            System.out.println("given arguments are = "+ i);
+        }
+    }
+    public static void main(String[] args) {
+        calc k = new calc();
+        k.add(2,5,6,8,9);
+        k.multiply(2,5,6,8,9);
+        k.display(2,5,6,8,9);
+    }
+}
