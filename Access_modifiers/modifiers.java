c2685832d9ffeb0238a9dc84b3c65cde1fb59891
kdpandey09 <pandeykaustubdutt@gmail.com>
2022-04-23 00:18:32 +0530

    updates

---
+++ Access_modifiers/modifiers.java
@@ -0,0 +1,30 @@
+package Access_modifiers;
+
+class twelfth {
+
+    public static void main(String[] args) {
+        modifiers B = new modifiers();
+        B._default_();
+        B._public_();
+        B._protected_();
+
+    }
+}
+
+public class modifiers {
+    void _default_() {
+        System.out.println("This is a Default access modifier");
+    }
+
+    public void _public_() {
+        System.out.println("This is a Public access modifier");
+    }
+
+    private void _private_() {
+        System.out.println("This is a Private acess modifier");
+    }
+
+    protected void _protected_() {
+        System.out.println("This is a Protected acess modifier");
+    }
+}
---
+++ Access_modifiers/modifiers1.java
@@ -0,0 +1,10 @@
+
+import Access_modifiers.modifiers;
+
+public class modifiers1 {
+    public static void main(String[] args) {
+        // thirteenth obj = new thirteenth();
+        modifiers obj = new modifiers();
+        obj._public_();
+    }
+}
