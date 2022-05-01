4400f79f9b485e78cfc93ea433f59cd2a50bc8e1
kdpandey09 <pandeykaustubdutt@gmail.com>
2022-05-01 20:18:49 +0530

    exception handling and its various features

---
+++ exceptionHandling.java
@@ -0,0 +1,21 @@
+/*Java program to implement exception handling and its various features (Kaustub Dutt Pandey)*/
+public class exceptionHandling {
+    static void fun() {
+        try {
+            throw new NullPointerException("demo");
+        } catch (NullPointerException e) {
+            System.out.println("Caught inside fun().");
+            throw e; // rethrowing the exception
+        }
+    }
+
+    public static void main(String args[]) {
+        try {
+            fun();
+        } catch (NullPointerException e) {
+            System.out.println("Caught in main.");
+        } finally {
+            System.out.println("Finally block executed");
+        }
+    }
+}
